37ab0e742021f66a1ce804834fc3016ff026aa8e
Andrew Cowie <andrew@operationaldynamics.com>
2018-06-15 23:18:47 +1000

    Comprehensive usage statement if book list not specified

--- src/RenderDocument.hs
+++ src/RenderDocument.hs
@@ -1,14 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# OPTIONS_HADDOCK hide, not-home #-}
+
 import Control.Monad (filterM)
+import Data.String.Here
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import System.Environment (getArgs)
+import System.Exit
 import System.FilePath.Posix (takeBaseName)
 import System.Directory (doesFileExist)
 import Text.Pandoc
-
-checkFile :: FilePath -> IO Bool
-checkFile file = doesFileExist file
 
 readFragment :: FilePath -> IO Pandoc
 readFragment file = do
@@ -25,10 +28,21 @@ produceResult name docs =
     T.writeFile (name ++ ".latex") result
 
 usage :: IO ()
-usage = putStrLn "publish <BookName.list>"
+usage = putStrLn [here|
+Usage:
+
+    publish <BookName.list>"
+
+where BookName will be used as the base name for the intermediate .latex file
+and the final output .pdf file. The list file should contain filenames, one per
+line, of the fragments you wish to render into a complete document.
+|]
 
 processBookFile :: [String] -> IO (String, [FilePath])
-processBookFile [] = usage >> error "No book file specified"
+processBookFile [] = do
+    putStrLn "Error: No book file specified"
+    usage
+    return ("",[])
 processBookFile (file:_) = do
     contents <- T.readFile file
 
