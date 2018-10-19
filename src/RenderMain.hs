23d4853ec87a5e8d67f0bad69426993ab073ee12
Andrew Cowie <andrew@operationaldynamics.com>
2018-10-19 22:17:47 +1100

    Parse command-line argument

--- src/RenderMain.hs
+++ src/RenderMain.hs
@@ -1,8 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main where
 
 import Core.Program
+import Core.Text
 import RenderDocument (program)
 
 
 main :: IO ()
-main = execute program
+main = do
+    context <- configure None (simple
+        [ Argument "bookfile" [quote|
+            The file containing the list of fragments making up this book.
+          |]
+        ])
+
+    executeWith context program
