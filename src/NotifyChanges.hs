2561274433a1256a98f415b94678044bccd6f381
Andrew Cowie <andrew@operationaldynamics.com>
2018-11-18 23:22:14 +1100

    Watch individual files rather than entire directory

--- src/NotifyChanges.hs
+++ src/NotifyChanges.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module NotifyChanges
 (
     waitForChange
@@ -7,6 +9,7 @@ where
 import Core.Program
 import Core.System
 import Control.Concurrent.MVar (MVar, newEmptyMVar, putMVar, readMVar)
+import qualified Data.ByteString.Char8 as C (pack)
 import System.INotify (EventVariety(..), Event(..), withINotify
     , addWatch, removeWatch)
 
@@ -14,20 +17,27 @@ import Environment
 
 waitForChange :: Program Env () 
 waitForChange = do
+    env <- getApplicationState
+    let files = intermediateFilenamesFrom env
+
     withContext $ \runProgram -> do
         block <- newEmptyMVar
         withINotify $ \notify -> do
-            watch <- addWatch
-                notify
-                [CloseWrite]
-                "."
-                (\event -> case event of
-                    Closed _ path _  -> do
-                        runProgram (debugS "path" path)
-                        putMVar block False
-                    _ -> return ())
+            -- setup inotifies
+            watches <- mapM (\file -> do
+                addWatch
+                    notify
+                    [CloseWrite]
+                    (C.pack file)
+                    (\event -> case event of
+                        Closed _ path _  -> do
+                            runProgram (debugS "path" path)
+                            putMVar block False
+                        _ -> return ()))
+                files
+
             -- wait
             readMVar block
-            removeWatch watch
-
+            -- cleanup
+            mapM_ removeWatch watches
 
