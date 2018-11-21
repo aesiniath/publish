e079d284547c85ce237df0d03f7f326b3a70841e
Andrew Cowie <andrew@operationaldynamics.com>
2018-11-22 00:58:27 +1100

    Write .target file in starting directory
    You're here, the resultant .pdf file is now written here, and just
    because you're reading from somewhere else doesn't mean you have write
    access there. So we write the .target here, rather than in the relative
    workdir.

--- src/RenderDocument.hs
+++ src/RenderDocument.hs
@@ -100,6 +100,10 @@ extractBookFile params =
 
 setupTargetFile :: FilePath -> Program Env ()
 setupTargetFile book = do
+    env <- getApplicationState
+    let start = startingDirectoryFrom env
+    let dotfile = start ++ "/.target"
+
     tmpdir <- liftIO $ catch
         (do
             dir' <- readFile dotfile
@@ -130,7 +134,6 @@ setupTargetFile book = do
             return [name]
         Just _      -> invalid
 
-    env <- getApplicationState
     let env' = env
             { intermediateFilenamesFrom = first
             , masterFilenameFrom = master
@@ -139,8 +142,6 @@ setupTargetFile book = do
             }
     setApplicationState env'
   where
-    dotfile = ".target"
-
     base = takeBaseName book -- "/directory/file.ext" -> "file"
 
     boom = userError "Temp dir no longer present"
