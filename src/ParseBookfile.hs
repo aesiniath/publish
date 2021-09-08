282bc5adb524bef3300d748987f91d0e61a98b19
Andrew Cowie <istathar@gmail.com>
2021-09-08 17:53:08 +1000

    Format code with Fourmolu

--- src/ParseBookfile.hs
+++ src/ParseBookfile.hs
@@ -15,48 +15,50 @@ __VERSION__ = 2
 
 parseMagicLine :: Parser Int
 parseMagicLine = do
-  void (char '%') <?> "first line to begin with % character"
-  void spaceChar <?> "a space character"
-  void (string "publish")
-  void spaceChar <?> "a space character"
-  void (char 'v') <?> "the character v and then a number"
-  v <- L.decimal <?> "the bookfile schema version number"
-  unless (v == __VERSION__) (fail ("currently recognized bookfile schema version is v" ++ show __VERSION__))
-  void newline
-  return v
+    void (char '%') <?> "first line to begin with % character"
+    void spaceChar <?> "a space character"
+    void (string "publish")
+    void spaceChar <?> "a space character"
+    void (char 'v') <?> "the character v and then a number"
+    v <- L.decimal <?> "the bookfile schema version number"
+    unless (v == __VERSION__) (fail ("currently recognized bookfile schema version is v" ++ show __VERSION__))
+    void newline
+    return v
 
 parseBeginLine :: Parser ()
-parseBeginLine = try $ label "begin marker" $ do
-  void (string "% begin")
-  void newline
+parseBeginLine = try $
+    label "begin marker" $ do
+        void (string "% begin")
+        void newline
 
 parseFileLine :: Parser FilePath
 parseFileLine = do
-  notFollowedBy (char '%')
-  file <- takeWhile1P (Just "line containing a filename") (/= '\n')
-  return file
+    notFollowedBy (char '%')
+    file <- takeWhile1P (Just "line containing a filename") (/= '\n')
+    return file
 
 parseEndLine :: Parser ()
-parseEndLine = try $ label "end marker" $ do
-  void (string "% end")
-  void newline
+parseEndLine = try $
+    label "end marker" $ do
+        void (string "% end")
+        void newline
 
 parseBlank :: Parser ()
 parseBlank = do
-  void (hidden (many newline))
+    void (hidden (many newline))
 
 parseBookfile :: Parser Bookfile
 parseBookfile = do
-  version <- parseMagicLine
-  preambles <- many (parseBlank *> parseFileLine <* parseBlank)
-  parseBeginLine
-  fragments <- many (parseBlank *> parseFileLine <* parseBlank)
-  parseEndLine
-  trailers <- many (parseBlank *> parseFileLine <* parseBlank)
-  return
-    Bookfile
-      { versionFrom = version,
-        preamblesFrom = preambles,
-        fragmentsFrom = fragments,
-        trailersFrom = trailers
-      }
+    version <- parseMagicLine
+    preambles <- many (parseBlank *> parseFileLine <* parseBlank)
+    parseBeginLine
+    fragments <- many (parseBlank *> parseFileLine <* parseBlank)
+    parseEndLine
+    trailers <- many (parseBlank *> parseFileLine <* parseBlank)
+    return
+        Bookfile
+            { versionFrom = version
+            , preamblesFrom = preambles
+            , fragmentsFrom = fragments
+            , trailersFrom = trailers
+            }
