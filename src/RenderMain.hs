eee1e27635278f7199388f6b416e1d00b6b0cece
Andrew Cowie <istathar@gmail.com>
2021-10-31 17:19:45 +1100

    Offer all three exporters

--- src/RenderMain.hs
+++ src/RenderMain.hs
@@ -84,6 +84,6 @@ main = do
                 ]
             )
 
-    context' <- initializeTelemetry [honeycombExporter] context
+    context' <- initializeTelemetry [consoleExporter,structuredExporter,honeycombExporter] context
 
     executeWith context' program
